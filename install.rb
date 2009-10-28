459791ba192f9268fcac2dc7562d0efbf50c3478
Elad Meidar <elad@eizesus.com>
2009-10-28 17:01:20 -0400

    Simple fix in install.rb

--- install.rb
+++ install.rb
@@ -15,10 +15,11 @@ dir = "stylesheets"
 end
 
 dir = "images"
-["blackbird_panel.png", "blackbird_icon.png"].each do |img_file|
+["blackbird_panel.png", "blackbird_icons.png"].each do |img_file|
 	dest_file = File.join(Rails.root, "public", dir, img_file)
 	src_file = File.join(File.dirname(__FILE__) , dir, img_file)
 	FileUtils.cp_r(src_file, dest_file)
 end
 puts "Images were copied to your applications public/images folder, if you wish to change that, remember to change any background-image reference on the blackbird.css file."
+
 puts "Files copied - Installation complete!"
