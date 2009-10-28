d9f5223c7c329329ff6e6a0b994ed6c9d8b8cc87
Elad Meidar <elad@eizesus.com>
2009-10-28 17:06:43 -0400

    Buggy syntax

--- lib/black_bird_helper.rb
+++ lib/black_bird_helper.rb
@@ -1,10 +1,9 @@
 module BlackBirdHelper
   
   # renders the blackbird CSS and Javascripts, if it's a devlopment env
-  def black_bird_tag
+  def include_black_bird
     if Rails.env == "development"
-      javascript_include_tag("blackbird")
-      stylesheet_link_tag("blackbird")
+      javascript_include_tag("blackbird") + stylesheet_link_tag("blackbird")
     end
   end
 end
