02c40f2795c5570e119ef1e42cf6528935eece78
Elad Meidar <elad@eizesus.com>
2009-10-30 11:59:52 -0400

    Now creating stubs for every other environment

--- lib/black_bird_helper.rb
+++ lib/black_bird_helper.rb
@@ -4,6 +4,8 @@ module BlackBirdHelper
   def include_black_bird
     if Rails.env == "development"
       javascript_include_tag("blackbird") + stylesheet_link_tag("blackbird")
+    else
+      "<script>var log = {toggle: function() {}, toggle: function() {}, debug:function () {}, info:function () {}, warn:function () {}, error:function () {}, profile:function () {}};</script>"
     end
   end
 end
